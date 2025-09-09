981ada6333d8ae4b99e6a589e38a5399af73810e
Thant Htet Aung <thanthtetaung12@gmail.com>
2025-09-09 13:05:09 +0630

    schdedule for download link expired

--- routes/console.php
+++ routes/console.php
@@ -2,15 +2,20 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Carbon\Carbon;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
 Schedule::call(function () {
-    Storage::disk('public')->delete(
-        collect(Storage::disk('public')->files('callLogsExport'))
-            ->filter(fn($file) => now()->diffInMinutes(Storage::disk('public')->lastModified($file)) > 15)
-            ->all()
-    );
+   $files = Storage::disk('public')->files('callLogsExport');
+
+    $oldFiles = collect($files)
+        ->filter(fn($file) => now()->diffInMinutes(Carbon::createFromTimestamp(Storage::disk('public')->lastModified($file))) > 1)
+        ->all();
+
+    if (!empty($oldFiles)) {
+        Storage::disk('public')->delete($oldFiles);
+    }
 })->everyMinute();
